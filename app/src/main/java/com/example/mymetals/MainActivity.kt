361f61797c1ea3d5e4f607d74ee8582f34246eed
Joel Parizi <joelparizi@Joels-MBP.attlocal.net>
2021-03-08 17:49:28 -0500

    Justin is learning Android with Nathaniel

--- app/src/main/java/com/example/mymetals/MainActivity.kt
+++ app/src/main/java/com/example/mymetals/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.example.mymetals
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
@@ -8,8 +9,6 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
-        Log.d(TAG, "onCreate: ")
 
     }
 }
